499d6005546fb2b45aab14564c46e8ed7a76e1d5
Shivam Gupta <shivamgupta02005@gmail.com>
2025-01-21 22:26:00 +0530

    fix cursor bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 function Home() {
-  return (
-    <>
-      <div className="bg-red-500 h-[500px]"></div>
-      <div className="bg-blue-500 h-[500px]"></div>
-      <div className="bg-red-500 h-[500px]"></div>
-    </>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
